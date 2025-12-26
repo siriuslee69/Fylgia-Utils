4cdced548f4db93d1850c727d51d87b0071192c4
SiriusLee69 <n1ghty@t1mx4.com>
2025-12-26 05:49:10 +0100

    xjorkf

---
+++ src/math/particleSwarm/MBnvPSO.nim
@@ -0,0 +1,116 @@
+import std/random, std/math, strutils
+type 
+    Solution = seq[uint64]
+    Individual = object
+        bestSolution: Solution #personal best 
+        lastSolution: Solution
+        newSolution: Solution
+    Swarm = object
+        individuals: seq[Individual] = @[]
+        bestSolutions: seq[Solution] = @[] #global best, has to be ordered somewhat 
+        velocity: Solution = @[]
+        alphas: array[3, float64] = [0.5, 0.5, 0.5]
+        A: array[3, Solution] = [@[], @[], @[]]
+
+randomize()
+
+# ---- Boolean Funcs for the Solution type / seq[uint8] ---- #
+
+proc `not`(X: Solution): Solution =
+    var 
+        i = 0
+        l = X.len()
+    while i < l:
+        result.add(not X[i])
+        i.inc()
+    
+proc `and`(X, Y: Solution): Solution =
+    echo "Trying to AND solutions failed. Solutions/seq[uint8]  must be the same length!"
+    var 
+        i = 0
+        l = X.len()
+    while i < l:
+        result.add( X[i] and Y[i] )
+        i.inc()
+        
+proc `or`(X,Y: Solution): Solution =
+    if X.len() != Y.len():
+        echo "Trying to OR solutions failed. Solutions/seq[uint8] must be the same length!"
+        return
+    var 
+        i = 0
+        l = X.len()
+    while i < l:
+        result.add( X[i] or Y[i] )
+        i.inc()
+        
+# ---- Efficient functions for generating bytes with certain bit probabilities ---- #
+
+
+# p = probability * 256 (0..256)
+# p=0 -> always 0, p=256 -> always 1
+proc bitProbability8(p: float64): uint8 =
+    if p >= 1.0:
+        return 0b1111_1111
+    result = 0
+    let
+        x = rand(uint64)   # 1 call
+    echo "hey"
+    let
+        t:uint8 = (255 * p).round().uint8()
+    echo $t
+    for i in 0..7:
+        let u = cast[uint8](x shr (i*8))
+        if u < t:      # threshold compare
+            result = result or (1'u8 shl i)
+
+proc bitProbability64(p: float64): uint64 =
+    for i in 0.. 7:
+        let
+            rand: uint64 = cast[uint64](bitProbability8(p))
+        echo $rand & "here is round number: " & $i
+        result = result or (rand shl (i*8))
+
+proc bitProbabilitySolution(solutionLenInBits: int64, p: float64): Solution =
+    ## This will always do 8 calls to random() at least, to fill an entire uint64, so there is a bit of overhead. 
+    ## Unless you provide solutions with exactly a multiple of 64 entries (bits),
+    ## it might make more sense to create a new function to make this more efficient
+    let 
+        l = (solutionLenInBits/64).ceil().int64()
+    echo $l
+    echo $(solutionLenInBits/64)
+    for i in 0.. (l-1):
+        echo "Now doing round: " & $i 
+        result.add(bitProbability64(p))
+
+echo $bitProbability8(0.4)
+echo $bitProbability64(0.4)
+let k = bitProbabilitySolution(64, 0.3)
+echo "AHA!"
+echo $(k[0])
+
+proc init(s: var Swarm, beta, alpha1: float64): void =
+    ## alpha0: Chance that a bit is taken from the NEGATION of the last personal solution of the individual
+    ## alpha1: Chance that a bit is taken from the last solution / particle stays at current position
+    ## alpha2: Chance that a bit is taken from the personal best vs being taken from the global best instead
+    ## Chances don't translate 1:1 since the actual update function is nested
+    s.alphas[0] = beta
+    s.alphas[1] = alpha1 
+    s.alphas[2] = 1 - alpha1
+
+proc update(s: var Swarm): void =
+    let
+        b = s.A[0]
+        a1 = s.A[1]
+        a2 = s.A[2]
+    for individual in s.individuals:
+        let
+            g = s.bestSolutions[0]
+            p = individual.bestSolution
+            x = individual.lastSolution
+
+            w = a2 and p or (not a2) and g
+            v = a1 and x or (not a1) and w
+            u = b and (not x) or (not b) and v
+
+
--- src/time/sleep.nim
+++ src/time/sleep.nim
@@ -1,9 +1,9 @@
-import std/monotimes, std/os
+import std/monotimes, std/os, std/math
 
 proc sleepMono*(milsecs: int): void =
     ## Sleeps in milsecs
     ## Uses the monotimes instead of an os sleep call
-    ## Margin of error is within 300_000 nanoseconds or 0.3 milliseconds
+    ## Margin of error is within 1500 nanoseconds or 0.0015 milliseconds
     ## More precise than the std/os sleep function
     let 
         a = getMonoTime().ticks
@@ -12,28 +12,57 @@ proc sleepMono*(milsecs: int): void =
         discard
     return
 
+template takeTime(body: untyped): int64 =
+    var 
+        b: MonoTime
+        c: int64
+        a: MonoTime = getMonoTime()
+    body
+    b = getMonoTime()
+    c = b.ticks - a.ticks
+    c
+
+template takeTimes(n: int64, body: untyped): seq[int64] =
+    var 
+        i: int64 = 0
+        times: seq[int64] = @[]
+    while i < n:
+        times.add(takeTime(body))
+        i.inc()
+    times
+
+template takeTimeAverage(n: int64, body: untyped): int64 =
+    let 
+        T:seq[int64] = n.takeTimes(body)
+    var 
+        a: int64
+        i: int64 = 0
+        sum: int64 = 0
+    while i < n:
+        sum = sum + T[i]
+        i.inc()
+    (sum/n).round().int64()
+
 when defined(test):
     
     import std/unittest, ../debugging/checks
+    suite "takeTime":
+        let k = takeTime: discard 5+10*100
+        echo $k
+        let K = 5.takeTimes: discard 5+10*100
+        echo $K
+        let a = 10.takeTimeAverage: echo "Mep map"
+        echo a
 
     suite "sleep":
         test "sleep + benchmark vs std/os":
             const sleeptime = 1000 #milseconds
-            var 
-                a: MonoTime 
-                b: MonoTime
-                q: int
-            a = getMonoTime()
-            sleepMono(sleeptime)
-            b = getMonoTime()
-            q = (b.ticks - (a.ticks + sleeptime*1_000_000)) 
-            echo a.ticks
-            echo b.ticks
+
+            var q: int64 
+            q = takeTime(sleepMono(sleepTime)) - (sleeptime * 1_000_000)
             echo "custom: " & $q
-            check(q <= 1500 ) #difference should be within 1500 nanoseconds or 0.0015 milliseconds
-            a = getMonoTime()
-            sleep(sleeptime)
-            b = getMonoTime()
-            q = (b.ticks - (a.ticks + sleeptime*1_000_000))
-            echo "std/os: " & $ q
+            check(q <= 2_000 ) 
+
+            q = takeTime(sleep(sleepTime)) - (sleeptime * 1_000_000)
+            echo "std/os: " & $q
             check(q <= 100_000_000 ) 
