f31fc1c792c1f29938a9ec773f928c350f580bb4
SiriusLee69 <n1ghty@t1mx4.com>
2026-01-02 13:32:30 +0100

    woops wrong order

--- src/streams/write.nim
+++ src/streams/write.nim
@@ -1,5 +1,5 @@
-proc toByteSeqBE*(x: uint64): seq[uint8] =
-    ## Converts a bigger uint to a byteseq in BE order
+proc toByteSeqLE*(x: uint64): seq[uint8] =
+    ## Converts a bigger uint to a byteseq in LE order
     if x < 255'u64:
         result.add( cast[uint8](x) )
     elif x < 65535'u64: #max uint16 size; also: multiplications are done at compile time (wrote them for better readability)
